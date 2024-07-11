8b94a1aa247ce354cf63feb18a1cb53faf3e671e
hamyubatler <tayyy.hy813@gmail.com>
2024-07-11 12:55:04 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/java_assignment1/Java_Assignment1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_assignment1;
+
+/**
+ *
+ * @author User
+ */
+public class Java_Assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
